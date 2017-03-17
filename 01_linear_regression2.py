5e776950c783a30cc35c57cbf555f7432bb5b34c
bcart21 <byclink@gmail.com>
2017-03-17 09:51:37 +0900

    asdf

--- 01_linear_regression2.py
+++ 01_linear_regression2.py
@@ -2,11 +2,11 @@ from __future__ import print_function
 
 import numpy
 import tensorflow as tf
-import tfvariable
+from tfvariable import TFVariable #!!
 import myplot
 
 
-class XXX:
+class Base:
     costs = []
     weights = []
     biases = []
@@ -31,37 +31,39 @@ class XXX:
         for item in self.logs:
             print(item)
 
+    def set_hypothesis(self):
+        return tf.add(tf.mul(x_data, self.W), self.b)
+
+
     def learning(self, x_data, y_data):
         sess = tf.Session()
 
-        #W = TFVariable.get_var_list(1)
-        #b = TFVariable.get_var_list(1)
-        W = tfvariable.TFVariable.get_var('weight')
-        b = tfvariable.TFVariable.get_var('bias')
+        W = TFVariable.get_var('weight')
+        b = TFVariable.get_var('bias')
 
         # tf Graph Input
         #X = tf.placeholder("float")
         #Y = tf.placeholder("float")
 
         #hypothesis = W * x_data + b
-        y_prime = tf.add(tf.mul(x_data, W), b)
-
-        error = tf.reduce_mean(tf.square(y_prime - y_data))
-
+        hypothesis = tf.add(tf.mul(x_data, W), b)
+        cost_func = tf.reduce_mean(tf.square(hypothesis - y_data))
         l_rate = tf.Variable(0.1)
         optimizer = tf.train.GradientDescentOptimizer(l_rate)
-        learning = optimizer.minimize(error)
+        learning = optimizer.minimize(cost_func)
 
         init = tf.global_variables_initializer()
         sess.run(init)
 
         print(sess.run(W))
+        self.weights.append(sess.run(W))
+        self.biases.append(sess.run(b))
 
         for step in range(2001):
             sess.run(learning) #, feed_dict={X: x_data, Y: y_data})
 
             if step % 40 == 0:
-                val_error = sess.run(error) #, feed_dict={X: x_data, Y: y_data})
+                val_error = sess.run(cost_func) #, feed_dict={X: x_data, Y: y_data})
                 val_weight = sess.run(W)
                 val_bias = sess.run(b)
 
@@ -74,14 +76,17 @@ class XXX:
                 self.weights.append(val_weight)
                 self.biases.append(val_bias)
 
-        print("Learning finished!")
+        print("Learned!")
 
 
-gildong = XXX()
+class Derived(Base):
+    tmp = None
+
+gildong = Base()
 x_data = [1., 2., 3.]
 y_data = [1., 2., 3.]
 gildong.learning(x_data, y_data)
-gildong.show_error()
-#gildong.show_weight()
+#gildong.show_error()
+gildong.show_weight()
 #gildong.show_bias()
-gildong.print_log()
+#gildong.print_log()
--- a.py
+++ a.py
@@ -23,3 +23,24 @@ sess = tf.Session()
 result = sess.run(d)
 print(result)
 
+
+
+class Animal():
+    def speak(self):
+        print("...")
+
+class Cat(Animal):
+    def speak(self):
+        print("meow")
+
+class Dog(Animal):
+    def speak(self):
+        print("woof")
+
+my_pets = [Dog(), Cat(), Dog()]
+
+for _pet in my_pets:
+     _pet.speak()
+
+
+
